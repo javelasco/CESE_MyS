a645e42f072aa1d98058770d07f0c7978f04510e
Jhonny Velasco <72809029+javelasco@users.noreply.github.com>
2022-12-14 10:48:45 -0500

    update

--- Fuentes C/mul_pf.c
+++ Fuentes C/mul_pf.c
@@ -10,17 +10,19 @@
 #define RECV_BUFFER_SIZE	1
 
 
-float res1;
-float opA1=1.1 ;
-float opB1=1.5 ;
+int res1 = 0;
+int opA1 = 0;
+int opB1 = 0;
+//int opA1=0x40200000;
+//int opB1=0xc1280000;
 
-float res2;
-float opA2=2.2 ;
-float opB2=3.1 ;
+int res2;
+int opA2=0xA0200000;
+int opB2=0xc3280000;
 
-float res3;
-float opA3=1.3 ;
-float opB3=1.7 ;
+int res3;
+int opA3=0x2A200000;
+int opB3=0xcE280000;
 
 
 XUartPs uart_ps;
@@ -47,6 +49,7 @@ int main (void) {
 	XUartPs_SetBaudRate(&uart_ps,115200);
 	//========================================================================
 
+	xil_printf("Bienvenidos!\r\n");
 	while(1)
 	{
 		data_rec=XUartPs_Recv(&uart_ps,&RecvBuffer,RECV_BUFFER_SIZE);
@@ -54,46 +57,45 @@ int main (void) {
 		{
 			data=RecvBuffer;
 
-			xil_printf("operacion %i:",RecvBuffer);
+			xil_printf("Operacion: %i \r\n",RecvBuffer);
 
 		}
 
-
 		switch(data){
-		case 49:
-				xil_printf("primera multiplcacion: \r\n");
+		case 49: // data es igual a #1
+			xil_printf("Primera multiplicacion: \r\n");
 
-			 	MUL_PF_IP_mWriteReg(XPAR_MUL_PF_IP_S_AXI_BASEADDR, MUL_PF_IP_S_AXI_SLV_REG0_OFFSET, opA1);
-			 	MUL_PF_IP_mWriteReg(XPAR_MUL_PF_IP_S_AXI_BASEADDR, MUL_PF_IP_S_AXI_SLV_REG1_OFFSET, opB1);
-			 	res1 = (float)MUL_PF_IP_mReadReg(XPAR_MUL_PF_IP_S_AXI_BASEADDR, MUL_PF_IP_S_AXI_SLV_REG2_OFFSET);
+			//MUL_PF_IP_mWriteReg(XPAR_MUL_PF_IP_S_AXI_BASEADDR, MUL_PF_IP_S_AXI_SLV_REG0_OFFSET, opA1);
+			//MUL_PF_IP_mWriteReg(XPAR_MUL_PF_IP_S_AXI_BASEADDR, MUL_PF_IP_S_AXI_SLV_REG1_OFFSET, opB1);
+			opA1 = MUL_PF_IP_mReadReg(XPAR_MUL_PF_IP_S_AXI_BASEADDR, MUL_PF_IP_S_AXI_SLV_REG0_OFFSET);
+			opB1 = MUL_PF_IP_mReadReg(XPAR_MUL_PF_IP_S_AXI_BASEADDR, MUL_PF_IP_S_AXI_SLV_REG1_OFFSET);
+			res1 = MUL_PF_IP_mReadReg(XPAR_MUL_PF_IP_S_AXI_BASEADDR, MUL_PF_IP_S_AXI_SLV_REG2_OFFSET);
 
-			 	 xil_printf("multiplicacion: %f * %f = %f\r\n", opA1, opB1, res1);
-
+			xil_printf(" --Multiplicacion: %x * %x = %x\r\n", opA1, opB1, res1);
 			break;
-		case 50:
-			xil_printf("segunda multiplcacion: \r\n");
+
+		case 50: // data es igual a #2
+			xil_printf("Segunda multiplicacion: \r\n");
 
 			MUL_PF_IP_mWriteReg(XPAR_MUL_PF_IP_S_AXI_BASEADDR, MUL_PF_IP_S_AXI_SLV_REG0_OFFSET, opA2);
 			MUL_PF_IP_mWriteReg(XPAR_MUL_PF_IP_S_AXI_BASEADDR, MUL_PF_IP_S_AXI_SLV_REG1_OFFSET, opB2);
-			res2 = (float) MUL_PF_IP_mReadReg(XPAR_MUL_PF_IP_S_AXI_BASEADDR, MUL_PF_IP_S_AXI_SLV_REG2_OFFSET);
+			res2 = MUL_PF_IP_mReadReg(XPAR_MUL_PF_IP_S_AXI_BASEADDR, MUL_PF_IP_S_AXI_SLV_REG2_OFFSET);
 
-			 xil_printf("multiplicacion: %f * %f = %f\r\n", opA2, opB2, res2);
-
+			xil_printf(" --Multiplicacion: %x * %x = %x\r\n", opA2, opB2, res2);
 			break;
 
-		case 51:
-			xil_printf("tercera multiplcacion: \r\n");
+		case 51: // data es igual a #3
+			xil_printf("Tercera multiplicacion: \r\n");
 
 			MUL_PF_IP_mWriteReg(XPAR_MUL_PF_IP_S_AXI_BASEADDR, MUL_PF_IP_S_AXI_SLV_REG0_OFFSET, opA3);
 			MUL_PF_IP_mWriteReg(XPAR_MUL_PF_IP_S_AXI_BASEADDR, MUL_PF_IP_S_AXI_SLV_REG1_OFFSET, opB3);
-			res3 = (float)MUL_PF_IP_mReadReg(XPAR_MUL_PF_IP_S_AXI_BASEADDR, MUL_PF_IP_S_AXI_SLV_REG2_OFFSET);
+			res3 = MUL_PF_IP_mReadReg(XPAR_MUL_PF_IP_S_AXI_BASEADDR, MUL_PF_IP_S_AXI_SLV_REG2_OFFSET);
 
-			xil_printf("multiplicacion: %d * %d = %d\r\n", opA3, opB3, res3);
-
+			xil_printf(" --Multiplicacion: %x * %x = %x\r\n", opA3, opB3, res3);
 			break;
 
 		default:
-			xil_printf("seleccione operacion");
+			//xil_printf("seleccione operacion. \r\n");
 			break;
 		}
 	}
